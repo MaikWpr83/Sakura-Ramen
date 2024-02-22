0efee47c757528996533f6df29cb1bf349e1f1fc
Maik Wypior <maikwypior83@outlook.com>
2024-02-22 13:21:08 +0100

    first commit

---
+++ übersetzen.js
@@ -0,0 +1,69 @@
+/*Übersetzung der Seite*/
+function deutsch(){   
+    /*Menü Abschnitt 1*/
+    document.getElementById('menu').innerHTML = 'Menü';
+    document.getElementById('locations').innerHTML = 'Standort';
+    document.getElementById('about').innerHTML = 'Über uns';
+    /*Überschrift 2 Abschnitt 1*/
+    document.getElementById('überschrift_2').innerHTML = 'Die besten Ramen der Stadt';
+    /*Überschrift 3 Abschnitt 2*/
+    document.getElementById('überschrift_3').innerHTML = 'Wie man bestellt';
+    /*Beschreibung Abschnitt 2*/
+    document.getElementById('nudel').innerHTML = 'Wähle deine Nudel';
+    document.getElementById('suppe').innerHTML = 'Wähle deine Suppe';
+    document.getElementById('eier').innerHTML = 'Füge Ihre Toppings hinzu';
+    /*Überschrift 4 Abschnitt 3*/
+    document.getElementById('überschrift_4').innerHTML = 'Unser Menü';
+    /*Menü Abschnitt 3*/
+    document.getElementById('chicken_menu_1').innerHTML = 'Huhn';
+    document.getElementById('beef_menu_1').innerHTML = 'Rindfleisch';
+    document.getElementById('shrimp_menu_1').innerHTML = 'Garnelen';
+    document.getElementById('chicken_menu_2').innerHTML = 'Huhn';
+    document.getElementById('beef_menu_2').innerHTML = 'Rindfleisch';
+    document.getElementById('shrimp_menu_2').innerHTML = 'Garnelen';
+    document.getElementById('chicken_menu_3').innerHTML = 'Huhn';
+    document.getElementById('beef_menu_3').innerHTML = 'Rindfleisch';
+    document.getElementById('shrimp_menu_3').innerHTML = 'Garnelen';
+    /*Maps-Überschrift Abschnitt 4*/
+    document.getElementById('maps_überschrift').innerHTML = 'Sie finden uns unter';   
+}
+
+/*Übersetzung der Seite*/
+function english(){ 
+    /*Menü Abschnitt 1*/  
+    document.getElementById('menu').innerHTML = 'Menu';
+    document.getElementById('locations').innerHTML = 'Locations';
+    document.getElementById('about').innerHTML = 'About';
+    /*Überschrift 2 Abschnitt 1*/
+    document.getElementById('überschrift_2').innerHTML = 'THE BEST RAMEN IN TOWN';
+    /*Überschrift 3 Abschnitt 2*/
+    document.getElementById('überschrift_3').innerHTML = 'HOW TO ORDER';
+    /*Beschreibung Abschnitt 2*/
+    document.getElementById('nudel').innerHTML = 'Pick your noodle';
+    document.getElementById('suppe').innerHTML = 'Pick your broth';
+    document.getElementById('eier').innerHTML = 'Add your toppings';
+    /*Überschrift 4 Abschnitt 3*/
+    document.getElementById('überschrift_4').innerHTML = 'OUR MENU';
+    /*Menü Abschnitt 3*/
+    document.getElementById('chicken_menu_1').innerHTML = 'CHICKEN';
+    document.getElementById('beef_menu_1').innerHTML = 'BEEF';
+    document.getElementById('shrimp_menu_1').innerHTML = 'SHRIMP';
+    document.getElementById('chicken_menu_2').innerHTML = 'CHICKEN';
+    document.getElementById('beef_menu_2').innerHTML = 'BEEF';
+    document.getElementById('shrimp_menu_2').innerHTML = 'SHRIMP';
+    document.getElementById('chicken_menu_3').innerHTML = 'CHICKEN';
+    document.getElementById('beef_menu_3').innerHTML = 'BEEF';
+    document.getElementById('shrimp_menu_3').innerHTML = 'SHRIMP';
+    /*Maps-Überschrift Abschnitt 4*/
+    document.getElementById('maps_überschrift').innerHTML = 'FIND US AT';  
+}
+
+
+
+
+
+
+
+
+
+
